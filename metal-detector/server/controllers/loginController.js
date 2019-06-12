b55839b9f90d069be20d9f8251241a26726a1480
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-06-12 12:06:38 -0700

    fixed query for verifying user login

--- metal-detector/server/controllers/loginController.js
+++ metal-detector/server/controllers/loginController.js
@@ -2,8 +2,9 @@ const createDB = require('../models/dbModel');
 
 module.exports = {
   verifyUser: (req, res, next) => {
+    console.log('in Verify user')
     const pool = createDB();
-    const verifyUserQuery = 'SELECT username, password FROM users_table (username, password) VALUES ($1, $2)';
+    const verifyUserQuery = 'SELECT username, password FROM users_table WHERE username=$1 AND password=$2';
     const verifyUserArray = [req.body.username, req.body.password];
     pool.query(verifyUserQuery, verifyUserArray, (err, result) => {
       if (err) {
