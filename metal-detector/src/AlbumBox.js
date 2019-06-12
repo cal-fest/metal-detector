7515e2cb50d2e0355c444ebc5080ad2369c56491
chetbaby <chet.hay@gmail.com>
2019-06-11 18:26:09 -0700

    added toggle state

--- metal-detector/src/AlbumBox.js
+++ metal-detector/src/AlbumBox.js
@@ -3,15 +3,19 @@ import React from "react";
 class AlbumBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
+    this.state = { isToggleOn: false };
     this.handleClick = this.handleClick.bind(this);
 
   }
   handleClick(e) {
     const albumBox = e.target;
     albumBox.style.display = 'block';
-    albumBox.style.color = 'lime';
-    albumBox.style.backgroundColor = 'white';
+    this.setState({ isToggleOn: !this.state.isToggleOn })
+    this.state.isToggleOn ?
+      albumBox.style.backgroundColor = 'white' :
+      albumBox.style.backgroundColor = 'black';
+    // albumBox.style.color = 'lime';
+
 
     let firstRect = albumBox.getBoundingClientRect();
     // let lastRect = detailItem.getBoundingClientRect();
